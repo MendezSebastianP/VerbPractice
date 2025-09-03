982828f8dc8963c02e5e2ffdf536281b65276d5f
Sebastian Mendez-Pineda <mendezpsebastian@gmail.com>
2025-09-03 15:58:14 +0200

    adding verbs app

--- myproject/verbs/services.py
+++ myproject/verbs/services.py
@@ -1,20 +1,114 @@
 from django.db import transaction
 from .models import Verb, UserVerb
 import random
+import math
+import numpy as np
+
 
 def init_user_verbs(user, n=10):
-    with transaction.atomic():
-        base = list(Verb.objects.order_by('id')[:n])
-        UserVerb.objects.bulk_create(
-            [UserVerb(user=user, verb=v, unlocked=True, probability=1.0) for v in base],
-            ignore_conflicts=True,
-        )
+	with transaction.atomic():
+		base = list(Verb.objects.order_by('id')[:n])
+		UserVerb.objects.bulk_create(
+			[UserVerb(user=user, verb=v, unlocked=True) for v in base],
+			ignore_conflicts=True,
+		)
 
 class verb_session():
 	def __init__(self, nverbs):
 		self.nverbs = nverbs
+		N_VERBS = 1000
+
 	def score_to_prob(self, scores):
-		total_score = sum(item['score'] for item in scores.values())
-		return {key: value['score'] / total_score for key, value in scores.items()}
-	def pick_next_verb(self):
-		pass
+		"""
+		Converts a dictionary of scores into probabilities by normalizing the values.
+
+		Args:
+			scores (dict): Dictionary with keys as IDs and values as scores.
+
+		Returns:
+			dict: Dictionary with keys as IDs and values as probabilities.
+		"""
+		prob = {}
+		sum_scores = sum(scores.values())
+		for key, value in scores.items():
+			prob[key] = value / sum_scores
+		return (prob)
+	
+	def next_verb(self, scores):
+		"""
+		Chooses the next verb based on probability derived from scores.
+
+		Args:
+			scores (Dict): A dictionary of scores.
+
+		Returns:
+			int: The selected verb key.
+		"""
+		prob = self.score_to_prob(scores)
+		verb_choose = np.random.choice(list(prob.keys()), p=list(prob.values()))
+		return (verb_choose)
+	
+	def verbs_session(self, scores, n):
+		"""
+		Selects a set number of verbs for a training session.
+
+		Args:
+			scores (Dict): A dictionary of scores.
+			n (int): Number of verbs to select.
+
+		Returns:
+			list: A list of selected verb keys.
+		"""
+		temp_scores = scores.copy()
+		selected_verbs = []
+		for i in range(n):
+			verb_choose = self.next_verb(temp_scores)
+			selected_verbs.append(verb_choose)
+			del temp_scores[verb_choose]
+		return (selected_verbs)
+	
+	def wr_answer(self, scores, index):
+		mult = 1.2 + ((self.N_VERBS - index + 1) / (self.N_VERBS - 1))
+		scores[index]['score'] *= mult
+		if (scores[index]['score'] > 10**5):
+			scores[index]['score'] = 10**5
+
+	def right_answer(self, scores, index):
+		scores[index]['score'] = scores[index]['score'] * (0.7)
+		if (scores[index]['score'] < 1):
+			scores[index]['score'] = 1
+
+	def hint(verb, times):
+		if (times <= len(verb)):
+				hinted = verb[0:times]
+				return (hinted)
+		else:
+				return -1
+	
+	def hint_answer(self, scores, index, n_hint, len_verb):
+		base_right_answer = (0.7)
+		base_right_answer = 1 - base_right_answer
+		if (n_hint >= len_verb/2):
+			self.wr_answer(scores, index)
+			return
+		elif (n_hint > 3 & n_hint < len_verb/2):
+			base_right_answer = 1
+		else:
+			base_right_answer = base_right_answer * (1 - (n_hint * (1/4)))
+			base_right_answer = 1 - base_right_answer
+		scores[index]['score'] = scores[index]['score'] * base_right_answer
+		if (scores[index]['score'] < 1):
+			scores[index]['score'] = 1
+
+	def next_scores(self, scores, index, response, verb = None, n_hint = None):
+		if (response == 1):
+			self.right_answer(scores, index)
+		elif (response == 2):
+			if (verb is None or n_hint is None):
+				print("Error (next_scores): response with hint need the verb and the number of hints")
+				return
+			self.hint_answer(scores, index, n_hint, len(verb))
+		elif (response == 0):
+			self.wr_answer(scores, index)
+		else:
+			print("Error (next_scores): not valid input")
--- myproject/verbs/views.py
+++ myproject/verbs/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import UserVerb
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url="/users/login/")
+def verbs_home(request):
+    # Get UserVerb objects for the current logged-in user
+    # (UserVerbs are initialized during user registration)
+    user_verbs = UserVerb.objects.filter(user=request.user).select_related('verb')
+    return render(request, 'verbs/verbs_home.html', {'verbs': user_verbs})
+
+# def post_page(request, slug):
+#     posts = Post.objects.get(slug=slug)
+#     return render(request, 'posts/post_page.html', { 'post' : posts})
+
+# @login_required(login_url="/users/login/")
+# def post_new(request):
+#     return render(request, 'posts/post_new.html')
